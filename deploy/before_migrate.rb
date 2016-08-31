412f7db4a74236b3c485564470d1ea038c94ad96
manish8856 <manish.kumar@crichq.com>
2016-08-31 11:07:09 +0530

    update

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,7 +3,7 @@ template "/tmp/file" do
   local true
   source "#{release_path}/deploy/templates/file.erb"
   user "deploy"
-  group "deploy"
+  group "www-data"
   mode "0644"
   action :create
   variables(hell: node['project'])
