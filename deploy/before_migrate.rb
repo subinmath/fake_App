81d30144480f5fe0ef9305714dfc3170fd9573d9
manish8856 <manish.kumar@crichq.com>
2016-08-31 10:57:21 +0530

    sdfgds

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,1 +1,12 @@
 package 'vim-nox'
+template "/tmp/file" do
+  local true
+  source "#{release_path}/deploy/templates/file.erb"
+  user "deploy"
+  group "deploy"
+  mode "0644"
+  action :create
+  variables {
+  	hell: node['project']
+  }
+end
